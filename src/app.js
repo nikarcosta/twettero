c0d6eebf7d034d5081825e2bd03b76c5a92af61b
nikarcosta <monique_r_costa@hotmail.com>
2023-01-07 16:27:06 -0300

    post tweets endpoint was added

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 const usuarios = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body;
@@ -16,6 +17,22 @@ app.post("/sign-up", (req, res) => {
         avatar
     });
 
+    res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+    const { username, tweet} = req.body;
+
+    const usuarioExistente = usuarios.find(usuario => usuario.username === username);
+
+    if(!usuarioExistente){
+        return res.status(401).send("UNAUTHORIZED");
+    }
+
+    tweets.push({
+        username,
+        tweet
+    });
     res.send("OK");
 });
 
